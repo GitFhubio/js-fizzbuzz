647a569ca51a1be56af9a0956fa54807a815e815
Fabio Petrone <fa610movic90@gmail.com>
2020-12-17 23:32:21 +0100

    oppure

--- js/script.js
+++ js/script.js
@@ -7,12 +7,13 @@
 for (var i = 1; i <= 100; i++) {
 
     if(i % 3 == 0 && i % 5 == 0) {
+      // potevo scrivere anche  % 15 == 0
         console.log('fizzbuzz');
     } else if (i % 5 == 0) {
         console.log('buzz');
     } else if(i % 3 == 0){
         console.log('fizz');
-    } else { 
+    } else {
         console.log(i);
     }
 }
